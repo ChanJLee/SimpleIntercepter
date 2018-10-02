78cec2fcd4dc3dde86db65584559d2042c6db299
ChanJLee <1355425625@qq.com>
2018-10-03 00:32:26 +0800

    refactor project struct

---
+++ src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/interpreter/Interpreter.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/interpreter/Interpreter.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/lexer/Lexer.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/lexer/Lexer.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/main.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/main.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/parser/Parser.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/parser/Parser.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/parser/ast/ASTNode.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/parser/ast/ASTNode.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/parser/ast/AssignStatementNode.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/parser/ast/AssignStatementNode.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/parser/ast/BinOpNode.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/parser/ast/BinOpNode.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/parser/ast/BlockNode.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/parser/ast/BlockNode.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/parser/ast/CompoundStatementNode.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/parser/ast/CompoundStatementNode.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/parser/ast/DeclarationsNode.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/parser/ast/DeclarationsNode.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/parser/ast/FormalParametersNode.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/parser/ast/FormalParametersNode.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/parser/ast/IntNumNode.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/parser/ast/IntNumNode.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/parser/ast/NoOpStatementNode.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/parser/ast/NoOpStatementNode.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/parser/ast/ProceduresNode.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/parser/ast/ProceduresNode.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/parser/ast/ProgramNode.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/parser/ast/ProgramNode.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/parser/ast/RealNumNode.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/parser/ast/RealNumNode.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/parser/ast/StatementNode.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/parser/ast/StatementNode.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/parser/ast/UnaryNode.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/parser/ast/UnaryNode.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/parser/ast/VarNode.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/parser/ast/VarNode.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/semantic/SemanticAnalyzer.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/semantic/SemanticAnalyzer.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/stream/char/CharStream.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/stream/char/CharStream.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/token/IdToken.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/token/IdToken.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/token/IntNumToken.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/token/IntNumToken.cpp.o"
+  "/Users/chan/ClionProjects/SimpleInterpreter/src/token/RealNumToken.cpp" "/Users/chan/ClionProjects/SimpleInterpreter/src/cmake-build-debug/CMakeFiles/SimpleInterpreter.dir/token/RealNumToken.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
